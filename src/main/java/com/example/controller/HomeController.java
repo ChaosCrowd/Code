11f959b58a1b531dfd9cd021feaba4ffdf684e00
adccccc <1203177935@qq.com>
2018-04-13 13:37:11 +0800

    demo

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping("")
+    public String home() {
+        //System.out.println("home page");
+        return "/html/home";
+    }
+
+    @RequestMapping("/login")
+    public String toLog() {
+        return "/html/login";
+    }
+
+}
---
+++ src/main/java/com/example/controller/RegisterController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/home/login")
+public class RegisterController {
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public String test(@RequestParam String username, @RequestParam String password, Model model){
+        if (username.equals("admin") && password.equals("1234")) {
+            model.addAttribute("username", username);
+            return "/html/succeed";
+        } else {
+            model.addAttribute("username", username);
+            return "/html/fail";
+        }
+
+    }
+}
